752fba7d2feeb21d364129cbeb63c41dafdd9dbd
Greta <matulionyte.greta@gmail.com>
2021-10-10 11:43:55 +0100

    add langing page with buttons and styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
+import logo from './logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Hackathon!</h1>
+      <div className="Logo-container">
+      <img src={logo} alt="Logo" /></div>
+       <h1>1 DAY = 1 STORY</h1>
+       <div className="Button-Group">
+       <div>
+       <button onClick={takeToDashboard}>DISCOVER </button> </div>
+       <div>
+       <button className="aboutButton" onClick={takeToAbout}>ABOUT</button> </div> </div>
       </header>
     </div>
   );
+}
+
+
+function takeToDashboard() {
+  alert('You clicked me!');
+}
+
+function takeToAbout() {
+  alert('You clicked me!');
 }
 
 export default App;
