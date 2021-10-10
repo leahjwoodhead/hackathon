5034bac60f2c619f8b37ba71c09052ba3518fe7d
Greta <matulionyte.greta@gmail.com>
2021-10-10 15:23:33 +0100

    add all files

---
+++ src/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  render(){
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', padding: 30, color: 'white' }}>
+                <div><h2>Dashboard </h2></div>
+            </div>
+    );
+  }
+}
+
+export default Dashboard;
---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import "./App.css";
+import logo from './logo.png';
+import history from "./history"
+
+function Home() {
+    return (
+    <div className="App">
+        <header className="App-header">
+            <div className="Logo-container">
+            <img src={logo} alt="Logo" /></div>
+            <h1>1 DAY = 1 STORY</h1>
+            <div className="Button-Group">
+            <form>
+            <button onClick={() => history.push('/Dashboard')}>DISCOVER</button>
+            </form>
+            <div>
+            <button  onClick={() => history.push('/About')}>ABOUT</button> </div> </div>
+            </header>
+             </div>
+              );
+  }
+
+export default Home;
