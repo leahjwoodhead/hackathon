c18ff4c26ef51e671e838afe36831e5db97de7c1
Greta <matulionyte.greta@gmail.com>
2021-10-10 15:11:33 +0100

    add router

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
+import React from 'react';
 import './App.css';
-import logo from './logo.png';
+import Routes from "./Routes";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <div className="Logo-container">
-      <img src={logo} alt="Logo" /></div>
-       <h1>1 DAY = 1 STORY</h1>
-       <div className="Button-Group">
-       <div>
-       <button onClick={takeToDashboard}>DISCOVER </button> </div>
-       <div>
-       <button className="aboutButton" onClick={takeToAbout}>ABOUT</button> </div> </div>
-      </header>
+      <Routes />
     </div>
   );
-}
-
-
-function takeToDashboard() {
-  alert('You clicked me!');
-}
-
-function takeToAbout() {
-  alert('You clicked me!');
 }
 
 export default App;
