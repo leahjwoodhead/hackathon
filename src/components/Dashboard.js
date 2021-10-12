5b4252f4f1df653686eeacafe7a5c18d1ee0f557
Leah Woodhead <leah.woodhead@panintelligence.com>
2021-10-12 22:44:29 +0100

    display data and add pagination

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,48 +3,104 @@ import Nav from './Nav';
 import styled from 'styled-components'
 import DashboardSideBar from './DashboardSideBar';
 import { data } from '../data/data'
+import Info from './Infomation';
 
 const MainDiv = styled.div`
   border: 1px solid white;
-  margin: 80px auto;
+  border-radius: 10px;
+  margin: 30px auto;
   width: 50%;
   height: auto;
+`
+
+const PageButtonLeft = styled.button`
+  color: white;
+  font-size: 10px;
+  border: none;
+`
+const PageButtonRight = styled.button`
+  color: white;
+  font-size: 10px;
+  border: none;
+  padding: 0px;
+`
+const ButtonDiv = styled.div`
+  display: flex;
+  margin: 0 auto;
+  width: 50%;
+  justify-content: space-between;
+  padding: 0px;
 `
 
 class Dashboard extends Component {
 
   state = {
-    topic: ""
+    topic: "",
+    data: "",
+    pages: 1,
+    p: 1
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const topic = this.props.match.params.topic
+    const {p} = this.state
+
+    if (prevProps !== this.props  || prevState.p !== p) {
+      if (topic === "Story") {
+        this.setState({topic: "Story", data: data[p].Story})
+      } else if (topic === "Podcast") {
+        this.setState({topic: "Podcast", data: data[p].Podcast})
+      } else if (topic === "Book") {
+        this.setState({topic: "Book", data: data[p].Book})
+      } else if (topic === "Music") {
+        this.setState({topic: "Music", data: data[p].Music})
+      } else if (topic === "Event") {
+        this.setState({topic: "Event", data: data[p].Event})
+      }
+    }
+
   }
 
   componentDidMount() {
     const topic = this.props.match.params.topic
+    const pages = data.length - 1
+    const p = data.length - 1
     if (topic === "Story") {
-      this.setState({topic: "story"})
+      this.setState({topic: "Story", data: data[pages].Story, pages, p})
     } else if (topic === "Podcast") {
-      this.setState({topic: "podcast"})
+      this.setState({topic: "Podcast", data: data[pages].Podcast, pages, p})
     } else if (topic === "Book") {
-      this.setState({topic: "book"})
+      this.setState({topic: "Book", data: data[pages].Book, pages, p})
     } else if (topic === "Music") {
-      this.setState({topic: "music"})
+      this.setState({topic: "Music", data: data[pages].Music, pages, p})
     } else if (topic === "Event") {
-      this.setState({topic: "event"})
+      this.setState({topic: "Event", data: data[pages].Event, pages, p})
     }
   }
 
+  handleClick(event) {
+    this.setState(currState => {
+      const newState = {...currState}
+      if (event.target.id === "yesterday") newState.p--
+      if (event.target.id === "next day") newState.p++
+      return newState
+    })
+  }
+
   render(){
+    const {pages, p} = this.state
     return (
       <div>
         <Nav/>
-        {console.log(this.props.match.params.topic, this.state.topicData)}
         <DashboardSideBar/>
+              <ButtonDiv>
+                  {(p > 0) ? <PageButtonRight id="yesterday" onClick={(event) => this.handleClick(event)}>yesterday</PageButtonRight> : <p></p>}
+                  {(p < pages) ? <PageButtonLeft id="next day" onClick={(event) => this.handleClick(event)}>next day</PageButtonLeft> : <p></p>}
+              </ButtonDiv>
           <MainDiv> 
-              <div style={{ display: 'flex', justifyContent: 'center', padding: 30, color: 'white' }}>
-                <div><h2>Dashboard </h2></div>
-            </div>
+                <Info data={this.state.data} topic={this.state.topic}/>
           </MainDiv>
       </div>
-    
     );
   }
 }
--- src/data/data.js
+++ src/data/data.js
@@ -1,87 +1,87 @@
 export const data = [
     {
-        "story":  { 
+        "Story":  { 
                 "text": "Ei ku jawohl en mi fertig hangen konnen gesagt. Dazwischen nachmittag ein eigentlich ist sog tat. Was dazwischen launischen das vorsichtig       verrichtet eigentlich wie ein. Wahres gerber gro ehe tal kannst. Naturlich in da nachgehen schwachem gegriffen ja. Gearbeitet bugeleisen birkendose neidgefuhl die das dienstmagd.",
                 "author": "Leah"
             },
-        "event": {
+        "Event": {
                 "title": "LGBT+ Dog Walk",
                 "location": "Meanwood Park",
                 "time": "Sat November 13th: 9am",
                 "description": "LGBT+ inclusive dog walk",
                 "link": "https://www.eventbrite.co.uk/e/leeds-lgbt-dog-walk-at-meanwood-park-tickets-148343698997?aff=ebdssbdestsearch"
         },
-        "podcast": {
+        "Podcast": {
                 "title": "Getting Curious",
                 "host": "Jonathan Van Ness",
                 "description": "JVN is inquisitive about a lot of things: cults, Lizzo, bees, the war in Yemen, the Romanovs, cash bail, artificial intelligence, the Beyoncé of renaissance painting, Brazil, menstrual cups, and figure skating, to name a few. Every week, the lusciously bearded Queer Eye guru sits down with a different specialist (from historians to surgeons and musicians) to spill tea and get answers to burning questions."
         },
-        "book": {
+        "Book": {
                 "title": "We Are Everywhere",
                 "author": "Matthew Riemer and Leighton Brown",
                 "description": "A rich and sweeping photographic history of the Queer Liberation Movement, from the creators and curators of the massively popular Instagram account @lgbt_history, released in time for the 50th anniversary of the Stonewall Riots."
         },
-        "music": {
+        "Music": {
                 "title": "Its Raining Men",
                 "artist": "The Weather Girls",
-                "year": 1983
+                "year": "1983"
 
         }
     },
     {
-        "story":  { 
+        "Story":  { 
                 "text": "Er la um bummelte herunter fraulein befehlen begehrte hindurch mu. Mitkommen schneider gewachsen zu spurenden wo magdebett sa. Esse denn dann ganz die auf trug steh sah. Ja im spurenden sorglosen argerlich zu. An begierig es streckte um sparlich pa konntest ansprach madchens. Buch mirs pa mu ture auch ware lieb da. Halt ihre mann den aber sag tief sehe dem. Einen weich blick hin die kunde mit habet. Kann da mein um leid.",
                 "author": "Greta"
             },
-        "event": {
+        "Event": {
                 "title": "Drag n Bass",
                 "location": "Wharf Chambers, Leeds",
                 "time": "Sat Nov 27th at 12:00am-04:00.",
                 "description": "An LGBT+ inclusive DnB Night",
                 "link": "https://allevents.in/leeds/drag-n-bass/300035854079"
         },
-        "podcast": {
+        "Podcast": {
                 "title": "Good 4 Thot",
                 "host": "Tommy Pico, Fran Tirado, Dennis Norris II, and Joe Osmundson",
                 "description": "A buoyant and brash deep dive into all things L, G, B, T, and Q (underwear parties to identity politics, theorist José Esteban Muñoz to 90s R&B) curated by a deliciously queer roundtable of multifaceted, multiracial movers and shakers. Winky recurring segments include 'Swipe Left, Swipe Right' (takes on cultural flashpoints, both highbrow and lowbrow) and 'Impure Thots' (cheeky remembrances of hot and/or bothered hook-ups). So grab a glass of rosé and get ready to dish."
         },
-        "book": {
+        "Book": {
                 "title": "Rubyfruit Jungle: A Novel",
                 "author": "RITA MAE BROWN",
                 "description": "Molly Bolt is the adoptive daughter of a poor Southern couple who makes her own way across America, finding love of all stripes in between.. It's a true, slightly steamy celebration of being true to yourself, whoever that may be"
         },
-        "music": {
+        "Music": {
                 "title": "Vogue",
                 "artist": "Madonna",
-                "year": 1900
+                "year": "1900"
         }
     },
     {
-        "story":  { 
+        "Story":  { 
                 "text": "Ort hin finde all drein euern klare. Hei warmen mag tal stille fellen. Uberging ri hausfrau du geholfen du allerlei. An dienstmagd launischen wohnzimmer bangigkeit mi. Uns ihm hemdarmel bedeckten geblendet kellnerin gut spurenden mannsbild. Te alle gewi gott lieb lief eins se. Dunner das see wie bliebe soviel. Trauen gefiel weiler war ebenso erfuhr her geduld neu. Indem im orten jahre dabei schon im ja hoher.",
                 "author": "Myles"
             },
-        "event": {
+        "Event": {
                 "title": "Leeds LGBT+ Coffee & Cake Cafe",
                 "location": "Mill Hill Chapel, City Square Leeds",
                 "time": "Sat Nov 27th at 12:00am-04:00.",
                 "description": "Join us on the evening for our accessible LGBT+ affirmative family friendly event to meet others, play board games, enjoy cake and refreshments or just for a friendly chatter with attendees. The event is staffed by Angels of Freedom volunteers, and funded by the incredible Brown & Blond bakery https://brownandblond.co.uk/",
                 "link": "https://www.evensi.uk/leeds-lgbt-coffee-cake-cafe-mill-hill-chapel/421694457"
         },
-        "podcast": {
+        "Podcast": {
                 "title": "Making Gay History",
                 "host": "Eric Marcus",
                 "description": "Emotional glimpses into the lives of unsung, if not entirely sidelined, trailblazers who made their mark on the LGBTQ civil rights movement—people like Vito Russo, Jean O'Leary, Bayard Rustin, and Del Martin. Marcus, author of a 2002 book also titled Making Gay History, pulls from his own recorded archives and conducts original interviews to collage together episodes on heroes who deserve robust chapters in the history books."
         },
-        "book": {
+        "Book": {
                 "title": "Fairest",
                 "author": "Meredith Talusan",
                 "description": "Fairest is a memoir about a precocious boy with albinism, a “sun child” from a rural Philippine village, who would grow up to become a woman in America. Throughout her journey, Talusan shares poignant and powerful episodes of desirability and love that will remind readers of works such as Call Me By Your Name and Giovanni’s Room. Her evocative reflections will shift our own perceptions of love, identity, gender, and the fairness of life."
         },
-        "music": {
+        "Music": {
                 "title": "Born This Way",
                 "artist": "Lady Gaga",
-                "year": 2011
+                "year": "2011"
         }
     },
 ];
