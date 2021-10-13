b1003dbf98a02beca2c0baa5d399b4a9418988d1
Greta <matulionyte.greta@gmail.com>
2021-10-13 08:04:29 +0100

    about page in working condition with styles

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,9 @@ function About() {
                         <h2>  Welcome!</h2>
                         <p>Hello! Thanks for joining us @ LGBTQ+ Daily - we are super happy to have you here.
 
-                        We created this space for people to have a platform to share their stories & interests and hopefully connect with likeminded individuals. In the world which can sometimes be unkind, our aim is to create a safe space, where everyone will be able to share their story and hear many more others. 
+                        We created this space for people to have a platform to share their stories & interests and hopefully connect with likeminded individuals. In the world which can sometimes be unkind, our aim is to create a safe space, where everyone will be able to share their story and hear many more others.Hello! Thanks for joining us @ LGBTQ+ Daily - we are super happy to have you here.
+
+We created this space for people to have a platform to share their stories & interests and hopefully connect with likeminded individuals. In the world which can sometimes be unkind, our aim is to create a safe space, where everyone will be able to share their story and hear many more others.  
                         </p>
                         <p>The idea behind the platform is very simple: each day we will be sharing a story of LGBTQ+ community members together with their favourite movies, songs, books & podcasts. Get inspired!</p>
                 </div>
