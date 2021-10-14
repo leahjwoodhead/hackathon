938fffdbf8448b14ab134783795b051b839fa542
Greta <matulionyte.greta@gmail.com>
2021-10-14 09:28:41 +0100

    add styles to comment section

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -4,11 +4,23 @@ import './styles/Comments.css'
 
 const InfoDiv = styled.div`
     border: 1px solid white;
-
-    width: 500px;
     color: white;
     font-size: 10px;
     margin: 0 auto;
+    margin: 0px auto;
+    color: white;
+    background-color:  grey;
+    width: 531px;
+    height: 70px; 
+    padding: 10px;
+    margin-bottom: 10px;
+    border-radius: 25px;
+    box-shadow: 3px 5px#E74A36;
+    border-bottom: 3px solid black;
+    border-top: 3px solid black;
+    border-right: 3px solid black;
+    border-left: 3px solid black;
+ 
     
 `
 
