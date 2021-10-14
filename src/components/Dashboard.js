7c3abf6b5b803becfd7bccead8919346c1c0a24b
Greta <matulionyte.greta@gmail.com>
2021-10-14 10:22:37 +0100

    fix comments

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -27,6 +27,23 @@ const InfoDiv = styled.div`
     margin: 0 auto;
     height: 40px;
     margin-top: 10px;
+    border: 1px solid white;
+    color: white;
+    font-size: 10px;
+    margin: 0 auto;
+    margin: 0px auto;
+    color: white;
+    background-color:  grey;
+    width: 531px;
+    height: 70px; 
+    padding: 10px;
+    margin-bottom: 10px;
+    border-radius: 25px;
+    box-shadow: 3px 5px#E74A36;
+    border-bottom: 3px solid black;
+    border-top: 3px solid black;
+    border-right: 3px solid black;
+    border-left: 3px solid black;
 `
 
 const Form = styled.form`
