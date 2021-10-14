07fdef58f33f8d8f65b10e49dca024427ea0e531
Greta <matulionyte.greta@gmail.com>
2021-10-14 08:19:21 +0100

    extra styling

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,7 @@ function About() {
     return (
         <div className="AboutPage">
             <Nav />
-            <div className="AboutContent">
-
-                <h2 className="WelcomeWord">  Welcome!</h2>
+                <h2>  Welcome!</h2>
                 <h3>Hello! Thanks for joining us @ LGBTQ+ Daily - we are super happy to have you here.
 
                     We created this space for people to have a platform to share their stories & interests and hopefully connect with likeminded individuals. In the world which can sometimes be unkind, our aim is to create a safe space, where everyone will be able to share their story and hear many more others.Hello! Thanks for joining us @ LGBTQ+ Daily - we are super happy to have you here.
@@ -17,7 +15,6 @@ function About() {
                 </h3>
                 <h4>The idea behind the platform is very simple: each day we will be sharing a story of LGBTQ+ community members together with their favourite movies, songs, books & podcasts. Get inspired!</h4>
             </div>
-        </div>
     );
 }
 
