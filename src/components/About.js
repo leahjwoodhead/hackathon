41f94db524a5b69d86e8801c998f1dbc412ef7f8
Greta <matulionyte.greta@gmail.com>
2021-10-15 07:46:34 +0100

    change about section

--- src/components/About.js
+++ src/components/About.js
@@ -6,15 +6,12 @@ function About() {
     return (
         <div className="AboutPage">
             <Nav />
-                <h2>  Welcome!</h2>
-                <h3>Hello! Thanks for joining us @ LGBTQ+ Daily - we are super happy to have you here.
-
-                    We created this space for people to have a platform to share their stories & interests and hopefully connect with likeminded individuals. In the world which can sometimes be unkind, our aim is to create a safe space, where everyone will be able to share their story and hear many more others.Hello! Thanks for joining us @ LGBTQ+ Daily - we are super happy to have you here.
-
-                    We created this space for people to have a platform to share their stories & interests and hopefully connect with likeminded individuals. In the world which can sometimes be unkind, our aim is to create a safe space, where everyone will be able to share their story and hear many more others.
-                </h3>
-                <h4>The idea behind the platform is very simple: each day we will be sharing a story of LGBTQ+ community members together with their favourite movies, songs, books & podcasts. Get inspired!</h4>
-            </div>
+            <h2>  Welcome!</h2>
+            <h3>Hello! Thanks for joining us @ LGBTQ+ Daily - we are super happy to have you here.
+                We created this space for people to have a platform to share their stories & interests and hopefully connect with likeminded individuals. In the world which can sometimes be unkind, our aim is to create a safe space, where everyone will be able to share their story and hear many more others. This plstform is a daily information hub wherein people can read inspirational stories from the community and receive suggestions for LGBTQ+ inclusive content such as events, books, podcasts and even a daily song suggestion.
+            </h3>
+            <h4>The idea behind the platform is very simple: each day, a new set of information will be displayed on the website, so there is always something new to learn. Get inspired!</h4>
+        </div>
     );
 }
 
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -13,13 +13,13 @@ import Comments from './Comments'
 const MainDiv = styled.div`
   margin: 30px auto;
   width: 50%;
-  height: 200px;
+  height: 50%;
 `
 
 const CommentsDiv = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 300px;
+  margin-top: 8%;
 `
 const InputComment = styled.input`
   width: 97%;
@@ -34,13 +34,12 @@ const InputComment = styled.input`
 
 const ButtonDiv = styled.div`
   display: flex;
-  margin: 0 auto;
-  width: 28.5%;
+  width: 22%;
   justify-content: space-between;
   padding: 0px;
   position: absolute;
   margin-left: 38%;
-  padding-top: 20px;
+  padding-top: 2%;
 `
 
 class Dashboard extends Component {
@@ -55,19 +54,19 @@ class Dashboard extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     const topic = this.props.match.params.topic
-    const {p} = this.state
+    const { p } = this.state
 
-    if (prevProps !== this.props  || prevState.p !== p) {
+    if (prevProps !== this.props || prevState.p !== p) {
       if (topic === "Story") {
-        this.setState({topic: "Story", data: data[p].Story, comments: data[p].Story.comments})
+        this.setState({ topic: "Story", data: data[p].Story, comments: data[p].Story.comments })
       } else if (topic === "Podcast") {
-        this.setState({topic: "Podcast", data: data[p].Podcast, comments: data[p].Podcast.comments})
+        this.setState({ topic: "Podcast", data: data[p].Podcast, comments: data[p].Podcast.comments })
       } else if (topic === "Book") {
-        this.setState({topic: "Book", data: data[p].Book, comments: data[p].Book.comments})
+        this.setState({ topic: "Book", data: data[p].Book, comments: data[p].Book.comments })
       } else if (topic === "Music") {
-        this.setState({topic: "Music", data: data[p].Music, comments: data[p].Music.comments})
+        this.setState({ topic: "Music", data: data[p].Music, comments: data[p].Music.comments })
       } else if (topic === "Event") {
-        this.setState({topic: "Event", data: data[p].Event, comments: data[p].Event.comments})
+        this.setState({ topic: "Event", data: data[p].Event, comments: data[p].Event.comments })
       }
     }
 
@@ -80,65 +79,53 @@ class Dashboard extends Component {
 
 
     if (topic === "Story") {
-      this.setState({topic: "Story", data: data[pages].Story, pages, p, comments: data[pages].Story.comments})
+      this.setState({ topic: "Story", data: data[pages].Story, pages, p, comments: data[pages].Story.comments })
     } else if (topic === "Podcast") {
-      this.setState({topic: "Podcast", data: data[pages].Podcast, pages, p, comments: data[pages].Podcast.comments})
+      this.setState({ topic: "Podcast", data: data[pages].Podcast, pages, p, comments: data[pages].Podcast.comments })
     } else if (topic === "Book") {
-      this.setState({topic: "Book", data: data[pages].Book, pages, p, comments: data[pages].Book.comments})
+      this.setState({ topic: "Book", data: data[pages].Book, pages, p, comments: data[pages].Book.comments })
     } else if (topic === "Music") {
-      this.setState({topic: "Music", data: data[pages].Music, pages, p, comments: data[pages].Music.comments})
+      this.setState({ topic: "Music", data: data[pages].Music, pages, p, comments: data[pages].Music.comments })
     } else if (topic === "Event") {
-      this.setState({topic: "Event", data: data[pages].Event, pages, p, comments: data[pages].Event.comments})
+      this.setState({ topic: "Event", data: data[pages].Event, pages, p, comments: data[pages].Event.comments })
     }
   }
 
   handleClick(event) {
     this.setState(currState => {
-      const newState = {...currState}
+      const newState = { ...currState }
       if (event.target.id === "yesterday") newState.p--
       if (event.target.id === "next day") newState.p++
       return newState
     })
   }
 
-
-  getDate(daysToSubtract){
-    let today = new Date();
-    const dd = String(today.getDate() - daysToSubtract).padStart(2, '0');
-    const mm = String(today.getMonth()).padStart(2, '0'); //January is 0!
-
-    today = dd + "/" + mm;
-    return today
-  }
-
-
   handleSubmission = (comment) => {
     this.setState(currState => {
-      const newState = {comments: [comment, ...currState.comments]}
+      const newState = { comments: [comment, ...currState.comments] }
       return newState
     })
   }
 
-
-  render(){
-    const {pages, p, topic} = this.state
+  render() {
+    const { pages, p, topic } = this.state
     return (
       <div className="MainDashboard">
-        <Nav/>
-        <DashboardSideBar/>
+        <Nav />
+        <DashboardSideBar />
         <ButtonDiv>
-                  {(p > 0) ? <img src={LeftIcon} alt="LeftIcon" height={75} id="yesterday" onClick={(event) => this.handleClick(event)}></img> : <p></p>}      
-                  {(p < pages) ? <img src={RightIcon} alt="RightIcon" height={75} id="next day" onClick={(event) => this.handleClick(event)}></img> : <p></p>}
-                  </ButtonDiv>
-          <MainDiv> 
-                <Info data={this.state.data} topic={this.state.topic}/>
-          </MainDiv>
-          <CommentsDiv>
-            {(this.state.data && (this.state.comments.length > 0)) ? this.state.comments.map(comment => {
-              return <Comments comment={comment}/>
-            }) : <p>This article does not have any comments</p>}
-            <CommentSubmission handleSubmission={this.handleSubmission}/>
-          </CommentsDiv>
+          {(p > 0) ? <img src={LeftIcon} alt="LeftIcon" height={75} id="yesterday" onClick={(event) => this.handleClick(event)}></img> : <p></p>}
+          {(p < pages) ? <img src={RightIcon} alt="RightIcon" height={75} id="next day" onClick={(event) => this.handleClick(event)}></img> : <p></p>}
+        </ButtonDiv>
+        <MainDiv>
+          <Info data={this.state.data} topic={this.state.topic} />
+        </MainDiv>
+        <CommentsDiv>
+          {(this.state.data && (this.state.comments.length > 0)) ? this.state.comments.map(comment => {
+            return <Comments comment={comment} />
+          }) : <p>This article does not have any comments</p>}
+          <CommentSubmission handleSubmission={this.handleSubmission} />
+        </CommentsDiv>
       </div>
     );
   }
